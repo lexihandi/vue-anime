218e31f53deaeeb5fa108e10ba7fe68e23ff0b79
lexyhandi <lexihandi@gmail.com>
2021-01-22 22:46:30 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="app">
+    <header>
+      <h1>The<Strong>Anime</Strong>Database</h1>
+      <form class="search-box">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Find your anime here..."
+          required
+        />
+      </form>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background-color: #f3f3f3;
+      box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 18px;
+
+      color: #313131;
+      font-size: 20px;
+
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.15);
+      }
+    }
+  }
+}
+</style>
